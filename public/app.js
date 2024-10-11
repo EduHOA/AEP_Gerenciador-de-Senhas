42f488cbc751b3ddd2299078b01e8ffaa841d202
Lucas Schuler Foppa <lucasfoppa@hotmail.com>
2024-10-11 16:08:49 -0300

    old friends

---
+++ public/app.js
@@ -0,0 +1,270 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const loading = document.getElementById('loading');
+    loading.style.display = 'none'; 
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const usernameDisplay = document.getElementById('username-display');
+    const username = localStorage.getItem('username');
+    if (username) {
+        usernameDisplay.textContent = `Bem-vindo, ${username}!`;
+    } else {
+        usernameDisplay.textContent = 'Usuário não identificado';
+    }
+});
+
+const loginForm = document.getElementById('login-form');
+if (loginForm) {
+  loginForm.addEventListener('submit', async (event) => {
+    event.preventDefault(); 
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const loading = document.getElementById('loading');
+    loading.style.display = 'none'; 
+
+    try {
+      const response = await fetch('/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        localStorage.setItem('username', username);
+
+        loading.style.display = 'block'; 
+
+        setTimeout(() => {
+          localStorage.setItem('token', data.token);
+          window.location.href = '/home.html';
+        }, 2000); 
+      } else {
+        alert(data.message || 'Erro no login');
+        loading.style.display = 'none'; 
+      }
+    } catch (error) {
+      console.error('Erro:', error);
+      alert('Erro no servidor, tente novamente mais tarde.');
+    } finally {
+        setTimeout(() => {
+          loading.style.display = 'none';
+        }, 2100); 
+    }
+  });
+}
+
+const registerForm = document.getElementById('register-form');
+if (registerForm) {
+  registerForm.addEventListener('submit', async (event) => {
+    event.preventDefault(); 
+
+    const username = document.getElementById('register-username').value;
+    const password = document.getElementById('register-password').value;
+
+    const successCard = document.querySelector('.success-card');
+    const loadingText = document.querySelector('.loading-text');
+
+    try {
+      const response = await fetch('/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        successCard.style.display = 'block';
+        loadingText.style.display = 'block';
+
+          setTimeout(() => {
+            successCard.classList.add('fade-out');
+
+            setTimeout(() => {
+              successCard.style.display = 'none';
+              successCard.classList.remove('fade-out');
+              window.location.href = 'index.html';
+            }, 500);
+          }, 3500);
+      } else {
+        successCard.style.display = 'none';
+        alert(data.message || 'Erro no registro');
+      }
+    } catch (error) {
+      console.error('Erro:', error);
+      alert('Erro no servidor, tente novamente mais tarde.');
+    }
+  });
+}
+
+const passwordForm = document.getElementById('password-form');
+if (passwordForm) {
+  passwordForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const url = document.getElementById('url').value;
+    const password = document.getElementById('site-password').value;
+
+    try {
+      const token = localStorage.getItem('token'); 
+      const response = await fetch('/add-password', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({ url, password }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        alert('Senha salva com sucesso!');
+        loadPasswords();
+      } else {
+        alert(data.message || 'Erro ao salvar senha');
+      }
+    } catch (error) {
+      console.error('Erro:', error);
+      alert('Erro no servidor, tente novamente mais tarde.');
+    }
+  });
+}
+
+async function loadPasswords() {
+  try {
+    const token = localStorage.getItem('token');
+    const response = await fetch('/get-passwords', {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    });
+
+    const passwords = await response.json();
+    const passwordList = document.getElementById('passwords-list');
+    passwordList.innerHTML = ''; 
+
+    passwords.forEach(password => {
+        const passwordItem = document.createElement('div');
+        passwordItem.innerHTML = `
+          <strong>URL:</strong> ${password.url} 
+          <br> 
+          <button class="view-password btn btn-primary" data-id="${password._id}">Ver Senha</button>
+          <button class="delete-password btn btn-primary" data-id="${password._id}">Excluir</button>
+        `;
+        passwordList.appendChild(passwordItem);
+      });
+      
+    const viewButtons = document.querySelectorAll('.view-password');
+    viewButtons.forEach(button => {
+      button.addEventListener('click', (e) => {
+        const passwordId = e.target.getAttribute('data-id');
+        showPasswordModal(passwordId);
+      });
+    });
+  } catch (error) {
+    console.error('Erro ao carregar senhas:', error);
+  }
+
+    const deleteButtons = document.querySelectorAll('.delete-password');
+    deleteButtons.forEach(button => {
+     button.addEventListener('click', (e) => {
+        const passwordId = e.target.getAttribute('data-id');
+        deletePassword(passwordId);
+       });
+    });
+}
+
+function showPasswordModal(passwordId) {
+    const modal = document.getElementById('view-password-modal');
+    modal.style.display = 'block';
+
+    const confirmButton = document.getElementById('confirm-view-password');
+    confirmButton.onclick = async () => {
+        const verifyPassword = document.getElementById('verify-password').value;
+        try {
+            const token = localStorage.getItem('token');
+            const response = await fetch(`/verify-password/${passwordId}`, {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ password: verifyPassword }),
+            });
+
+            const data = await response.json();
+            if (response.ok) {
+                document.getElementById('encrypted-password-display').innerText = `Senha Descriptografada: ${data.decryptedPassword}`;
+            } else {
+                alert(data.message || 'Senha incorreta');
+            }
+        } catch (error) {
+            console.error('Erro:', error);
+            alert('Erro no servidor, tente novamente mais tarde.');
+        }
+    };
+
+    const cancelButton = document.getElementById('cancel-view-password');
+    cancelButton.onclick = () => {
+        modal.style.display = 'none';
+        document.getElementById('verify-password').value = '';
+        document.getElementById('encrypted-password-display').innerText = '';
+    };
+
+    const closeButton = document.querySelector('.close');
+    closeButton.onclick = () => {
+        modal.style.display = 'none';
+        document.getElementById('verify-password').value = '';
+        document.getElementById('encrypted-password-display').innerText = '';
+    };
+
+    window.onclick = (event) => {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+            document.getElementById('verify-password').value = '';
+            document.getElementById('encrypted-password-display').innerText = '';
+        }
+    };
+}
+
+  
+
+async function deletePassword(passwordId) {
+    if (!confirm('Você tem certeza que deseja excluir esta senha?')) return;
+  
+    try {
+      const token = localStorage.getItem('token');
+      const response = await fetch(`/delete-password/${passwordId}`, {
+        method: 'DELETE',
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      });
+  
+      const data = await response.json();
+  
+      if (response.ok) {
+        alert('Senha excluída com sucesso!');
+        loadPasswords(); 
+      } else {
+        alert(data.message || 'Erro ao excluir a senha');
+      }
+    } catch (error) {
+      console.error('Erro ao excluir senha:', error);
+      alert('Erro no servidor, tente novamente mais tarde.');
+    }
+  }
+  
+if (window.location.pathname === '/home.html') {
+  loadPasswords();
+}
---
+++ server.js
@@ -0,0 +1,173 @@
+const express = require('express'); 
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const crypto = require('crypto');
+
+dotenv.config();  
+
+const app = express();  
+
+app.use(express.static('public')); 
+app.use(express.json());  
+app.use(cors());  
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Conectado ao MongoDB'))
+  .catch((error) => console.error('Erro ao conectar ao MongoDB:', error));
+
+const User = mongoose.model('User', new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+}));
+
+const Password = mongoose.model('Password', new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // Relacionado ao usuário
+  url: { type: String, required: true },
+  encryptedPassword: { type: String, required: true },
+}));
+
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ message: 'Usuário já existe' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({ username, password: hashedPassword });
+    await newUser.save();
+
+    res.status(201).json({ message: 'Usuário registrado com sucesso!' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Erro no servidor, tente novamente mais tarde.' });
+  }
+});
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+
+  if (!user) return res.status(400).json({ message: 'Usuário não encontrado' });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) return res.status(400).json({ message: 'Senha incorreta' }) ;
+
+  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+  res.json({ token });
+});
+
+const authMiddleware = (req, res, next) => {
+  const token = req.header('Authorization')?.replace('Bearer ', '');
+
+  if (!token) {
+    return res.status(401).json({ message: 'Acesso negado. Token não fornecido.' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decoded.userId; 
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Token inválido.' });
+  }
+};
+
+app.post('/add-password', authMiddleware, async (req, res) => {
+    const { url, password } = req.body;
+  
+    //essa parte peguei no chat que negócio de louco
+    try {
+      const algorithm = 'aes-256-cbc';
+      const key = crypto.randomBytes(32); // Chave de 32 bytes
+      const iv = crypto.randomBytes(16); // Vetor de inicialização
+  
+      const cipher = crypto.createCipheriv(algorithm, key, iv);
+      let encrypted = cipher.update(password, 'utf8', 'hex');
+      encrypted += cipher.final('hex');
+  
+      const newPassword = new Password({
+        userId: req.userId,
+        url,
+        encryptedPassword: `${iv.toString('hex')}:${encrypted}:${key.toString('hex')}`, 
+      });
+      await newPassword.save();
+  
+      res.status(201).json({ message: 'Senha salva com sucesso!' });
+    } catch (error) {
+      console.error('Erro ao salvar senha:', error);
+      res.status(500).json({ message: 'Erro no servidor ao salvar a senha' });
+    }
+  });
+  
+app.get('/get-passwords', authMiddleware, async (req, res) => {
+  try {
+    const passwords = await Password.find({ userId: req.userId });
+    res.json(passwords);
+  } catch (error) {
+    console.error('Erro ao obter senhas:', error);
+    res.status(500).json({ message: 'Erro no servidor ao buscar as senhas' });
+  }
+});
+
+app.post('/verify-password/:id', authMiddleware, async (req, res) => {
+    const { id } = req.params;
+    const { password } = req.body;
+  
+    try {
+      const passwordEntry = await Password.findById(id).populate('userId');
+  
+      if (!passwordEntry) {
+        return res.status(404).json({ message: 'Senha não encontrada' });
+      }
+  
+      const user = await User.findById(passwordEntry.userId._id);
+      const isMatch = await bcrypt.compare(password, user.password);
+  
+      if (!isMatch) {
+        return res.status(400).json({ message: 'Senha incorreta' });
+      }
+
+      //peguei a criptografia que o chat definiu e defini tambem como descriptografia
+      const [iv, encrypted, key] = passwordEntry.encryptedPassword.split(':');
+      const algorithm = 'aes-256-cbc';
+  
+      const decipher = crypto.createDecipheriv(algorithm, Buffer.from(key, 'hex'), Buffer.from(iv, 'hex'));
+      let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+      decrypted += decipher.final('utf8');
+  
+      res.json({ decryptedPassword: decrypted });
+    } catch (error) {
+      console.error('Erro ao verificar senha:', error);
+      res.status(500).json({ message: 'Erro no servidor ao verificar a senha' });
+    }
+  });
+  
+app.delete('/delete-password/:id', authMiddleware, async (req, res) => {
+    const { id } = req.params;
+  
+    try {
+      const passwordEntry = await Password.findByIdAndDelete(id);
+  
+      if (!passwordEntry) {
+        return res.status(404).json({ message: 'Senha não encontrada' });
+      }
+  
+      res.json({ message: 'Senha excluída com sucesso!' });
+    } catch (error) {
+      console.error('Erro ao excluir senha:', error);
+      res.status(500).json({ message: 'Erro no servidor ao excluir a senha' });
+    }
+  });
+  
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
